6a9ca0472f12505b542ecbffd15bfa4172e1b8bb
shindt1969 <shindt1969@gmail.com>
2022-09-29 11:05:34 +0800

    0929ting

--- src/views/Dolist4.vue
+++ src/views/Dolist4.vue
@@ -4,7 +4,7 @@ a-list(item-layout="horizontal", :data-source="datas")
     a-list-item
      template(#actions)
        a(key='list-loadmore-edit') edit
-       a(key='list-loadmore-more') more
+       a(key='list-loadmore-more' @click="removeItem(index)") delete
      div(style='margin: 1px; width : 1px; font-size:2px; margin-top: 80px; margin-right:1px;  ') {{item.created_at}}  
 
      a-skeleton(avatar='' :title='false' :loading='!!item.loading' active='')
@@ -13,11 +13,6 @@ a-list(item-layout="horizontal", :data-source="datas")
            a(href='https://www.antdv.com/') {{ item.target_name }}
          template(#avatar)
            a-avatar(src="https://joeschmoe.io/api/v1/random") 
-
-
-
-
-
 
 
 </template>
@@ -49,12 +44,19 @@ export default defineComponent({
           }
         });
     };
+
+
+      const removeItem = (index) => {  //移除訊息
+        datas.value.splice(index, 1)
+     } 
+
+
     onMounted(() => {
       getnote();
     });
 
     return {
-      datas,
+      datas,removeItem
     };
   },
 });
